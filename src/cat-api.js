ef3f71a0058a191e88981dac50657efe2a549b46
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2023-10-24 01:28:36 +0300

    dz10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const url = 'https://api.thecatapi.com/v1';
 const api_key =
-  'live_vvn0MJinoqW8XlzjTJigrKozl9H2hW1gVLfb0Jtx3x5h4e1VeQR1W0AtCmxsyGMW ';
+  'live_vvn0MJinoqW8XlzjTJigrKozl9H2hW1gVLfb0Jtx3x5h4e1VeQR1W0AtCmxsyGMW';
 
 export function fetchBreeds() {
   return fetch(`${url}/breeds?api_key=${api_key}`).then(response =>
